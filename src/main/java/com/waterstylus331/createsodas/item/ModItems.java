e1c19c14f636f8299b56b3d016acb831a62ad99f
Sri <saisrikara5@gmail.com>
2024-08-09 18:32:24 -0400

    Added sassafras seed and related items

--- src/main/java/com/waterstylus331/createsodas/item/ModItems.java
+++ src/main/java/com/waterstylus331/createsodas/item/ModItems.java
@@ -1,10 +1,12 @@
 package com.waterstylus331.createsodas.item;
 
 import com.waterstylus331.createsodas.CreateSodas;
+import com.waterstylus331.createsodas.block.ModBlocks;
 import com.waterstylus331.createsodas.fluid.ModFluids;
 import com.waterstylus331.createsodas.item.drinks.*;
 import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemNameBlockItem;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -32,6 +34,15 @@ public class ModItems {
     public static final RegistryObject<Item> EMPTY_UNPRINTED_CAN = ITEMS.register("empty_unprinted_can",
             () -> new Item(new Item.Properties().stacksTo(64)));
 
+    public static final RegistryObject<Item> SASSAFRAS_SEEDS = ITEMS.register("sassafras_seeds",
+            () -> new ItemNameBlockItem(ModBlocks.SASSAFRAS_PLANT.get(), new Item.Properties()));
+
+    public static final RegistryObject<Item> SASSAFRAS = ITEMS.register("sassafras",
+            () -> new Item(new Item.Properties().stacksTo(64)));
+
+    public static final RegistryObject<Item> SASSAFRAS_ROOT = ITEMS.register("sassafras_root",
+            () -> new Item(new Item.Properties().stacksTo(64)));
+
     public static final RegistryObject<Item> EMPTY_ROOT_BEER = ITEMS.register("empty_root_beer",
             () -> new Item(new Item.Properties().stacksTo(64)));
 
@@ -49,6 +60,16 @@ public class ModItems {
 
     public static final RegistryObject<Item> DR_PEPPER_BUCKET = ITEMS.register("dr_pepper_bucket",
             () -> new BucketItem(ModFluids.SOURCE_DR_PEPPER, new Item.Properties().craftRemainder(Items.BUCKET).stacksTo(1)));
+
+    public static final RegistryObject<Item> EMPTY_GINGER_ALE = ITEMS.register("empty_ginger_ale",
+            () -> new Item(new Item.Properties().stacksTo(64)));
+
+    public static final RegistryObject<Item> GINGER_ALE = ITEMS.register("ginger_ale",
+            () -> new GingerAleItem(new Item.Properties().stacksTo(64)));
+
+
+    public static final RegistryObject<Item> GINGER_ALE_BUCKET = ITEMS.register("giner_ale_bucket",
+            () -> new BucketItem(ModFluids.SOURCE_GINGER_ALE, new Item.Properties().craftRemainder(Items.BUCKET).stacksTo(1)));
 
     public static void register(IEventBus bus) {
         ITEMS.register(bus);
