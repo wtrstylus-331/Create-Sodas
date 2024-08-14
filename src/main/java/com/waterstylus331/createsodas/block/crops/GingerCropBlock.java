ba2551de7d2a6ed9024d54f93b3be0e922ae1469
Sri <saisrikara5@gmail.com>
2024-08-13 22:42:57 -0400

    Created ginger crop class

---
+++ src/main/java/com/waterstylus331/createsodas/block/crops/GingerCropBlock.java
@@ -0,0 +1,39 @@
+package com.waterstylus331.createsodas.block.crops;
+
+import com.waterstylus331.createsodas.item.ModItems;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+
+public class GingerCropBlock extends CropBlock {
+    public static final int maxAge = 4;
+    public static final IntegerProperty age = BlockStateProperties.AGE_4;
+
+    public GingerCropBlock(Properties p_52247_) {
+        super(p_52247_);
+    }
+
+    @Override
+    protected ItemLike getBaseSeedId() {
+        return ModItems.GINGER_ITEM.get();
+    }
+
+    @Override
+    protected IntegerProperty getAgeProperty() {
+        return age;
+    }
+
+    @Override
+    public int getMaxAge() {
+        return maxAge;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> stateBuilder) {
+        stateBuilder.add(age);
+    }
+}
